a95b8122355bed0faf365e9017cfb6743c27a08a
iliyan85 <iliyan85@gmail.com>
2016-12-18 03:18:16 +0200

    Add files via upload

--- parser.php
+++ parser.php
@@ -1,37 +1,17 @@
-<?php 
+<?php
+function get_string_between($string, $start, $end){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+}
 
-$myfile = file_get_contents("http://www.operasofia.bg/component/k2/repertoire");
 
+//$myfile = file_get_contents('http://www.operasofia.bg/component/k2/repertoire');
+ $opera = file_get_contents('opera');
+ $strip_opera = strip_tags($opera);
+ $parsed = get_string_between($strip_opera, 'КалендарДекември', 'Банер EasyPAY');
 
-
-//$ch = curl_init("http://www.operasofia.bg/component/k2/repertoire");
-//$fp = fopen("example_homepage.txt", "w");
-
-//curl_setopt($ch, CURLOPT_FILE, $fp);
-//curl_setopt($ch, CURLOPT_HEADER, 0);
-
-//curl_exec($ch);
-//curl_close($ch);
-
-//fclose($fp);
-
-/*
-$myfile = fopen("results.txt", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("webdictionary.txt"));
-
-$xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
-print_r($xml);
-
-$str = "That's the name of the event.";
-$name = explode(" ",$str);
-
-$str1 = "That's the place of the event.";
-$place = explode(" ",$str);
-
-$strtime = '10/16/2003';
-$strtime = str_replace('/', '-', $strtime);
-$date1 = strtotime($strtime);
-
-fclose($myfile);
-*/
-?>
+ echo '<pre>'.$parsed.'</pre>';
